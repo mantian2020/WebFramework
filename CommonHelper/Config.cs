79acf7e71b6501b29add2a23a920f3d3e8cb2376
mantian2020 <1665767273@qq.com>
2014-12-02 19:51:14 +0800

    添加生成MachineKey

--- CommonHelper/Config.cs
+++ CommonHelper/Config.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,37 @@ namespace CommonHelper
         public static string GetAppSettings(string key)
         {
             return ConfigurationManager.AppSettings[key];
+        }
+        /// <summary>
+        /// 生成MachineKey
+        /// </summary>
+        /// <returns></returns>
+        public static string GenerateMachineKey()
+        {
+            String[] commandLineArgs = System.Environment.GetCommandLineArgs();
+            string decryptionKey = CreateKey(System.Convert.ToInt32(commandLineArgs[1]));
+            string validationKey = CreateKey(System.Convert.ToInt32(commandLineArgs[2]));
+            return string.Format("<machineKey validationKey=\"{0}\" decryptionKey=\"{1}\" validation=\"SHA1\"/>",
+                validationKey, decryptionKey);
+        }
+
+        private static String CreateKey(int numBytes)
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buff = new byte[numBytes];
+            rng.GetBytes(buff);
+            return BytesToHexString(buff);
+        }
+
+        private static string BytesToHexString(byte[] bytes) 
+        {
+            StringBuilder hexString = new StringBuilder(64);
+
+            for (int counter = 0; counter < bytes.Length; counter++) 
+            {
+                hexString.Append(String.Format("{0:X2}", bytes[counter]));
+            }
+            return hexString.ToString();
         }
     }
 }
