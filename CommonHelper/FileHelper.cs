cc4b998663f5f12c936f784f6297cc948fc6e6fb
mantian2020 <1665767273@qq.com>
2014-12-03 18:20:57 +0800

    添加CacheFramework

---
+++ CommonHelper/FileHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CommonHelper
+{
+    public class FileHelper
+    {
+        /// <summary>
+        /// 返回文件是否存在
+        /// </summary>
+        /// <param name="filename">文件名</param>
+        /// <returns>是否存在</returns>
+        public static bool FileExists(string filename)
+        {
+            return System.IO.File.Exists(filename);
+        }
+        /// <summary>
+        /// 获得当前绝对路径
+        /// </summary>
+        /// <param name="strPath">指定的路径</param>
+        /// <returns>绝对路径</returns>
+        public static string GetMapPath(string strPath)
+        {
+            if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Server.MapPath(strPath);
+            }
+            else //非web程序引用
+            {
+                strPath = strPath.Replace("/", "\\");
+                if (strPath.StartsWith("\\"))
+                {
+                    strPath = strPath.Substring(strPath.IndexOf('\\', 0)).TrimStart('\\');
+                }
+                return System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory.Replace(@"bin\Debug\", ""), strPath);
+            }
+        }
+    }
+}
---
+++ CommonHelper/StringHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CommonHelper
+{
+    public class StringHelper
+    {
+        /// <summary>
+        /// 字段串是否为Null或为""(空)
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool StrIsNullOrEmpty(string str)
+        {
+            if (str == null || str.Trim() == string.Empty)
+                return true;
+
+            return false;
+        }
+        /// <summary>
+        /// 分割字符串
+        /// </summary>
+        public static string[] SplitString(string strContent, string strSplit)
+        {
+            if (!StrIsNullOrEmpty(strContent))
+            {
+                if (strContent.IndexOf(strSplit) < 0)
+                    return new string[] { strContent };
+
+                return Regex.Split(strContent, Regex.Escape(strSplit), RegexOptions.IgnoreCase);
+            }
+            else
+                return new string[0] { };
+        }
+        /// <summary>
+        /// 合并字符
+        /// </summary>
+        /// <param name="source">要合并的源字符串</param>
+        /// <param name="target">要被合并到的目的字符串</param>
+        /// <param name="mergechar">合并符</param>
+        /// <returns>合并到的目的字符串</returns>
+        public static string MergeString(string source, string target)
+        {
+            return MergeString(source, target, ",");
+        }
+
+        /// <summary>
+        /// 合并字符
+        /// </summary>
+        /// <param name="source">要合并的源字符串</param>
+        /// <param name="target">要被合并到的目的字符串</param>
+        /// <param name="mergechar">合并符</param>
+        /// <returns>并到字符串</returns>
+        public static string MergeString(string source, string target, string mergechar)
+        {
+            if (StrIsNullOrEmpty(target))
+                target = source;
+            else
+                target += mergechar + source;
+
+            return target;
+        }
+        /// <summary>
+        /// 判断给定的字符串数组(strNumber)中的数据是不是都为数值型
+        /// </summary>
+        /// <param name="strNumber">要确认的字符串数组</param>
+        /// <returns>是则返加true 不是则返回 false</returns>
+        public static bool IsNumericArray(string[] strNumber)
+        {
+            if (strNumber == null)
+                return false;
+
+            if (strNumber.Length < 1)
+                return false;
+
+            foreach (string id in strNumber)
+            {
+                if (!IsNumeric(id))
+                    return false;
+            }
+            return true;
+        }
+        /// <summary>
+        /// 判断对象是否为Int32类型的数字
+        /// </summary>
+        /// <param name="Expression"></param>
+        /// <returns></returns>
+        public static bool IsNumeric(string expression)
+        {
+            if (expression != null)
+            {
+                string str = expression;
+                if (str.Length > 0 && str.Length <= 11 && Regex.IsMatch(str, @"^[-]?[0-9]*[.]?[0-9]*$"))
+                {
+                    if ((str.Length < 10) || (str.Length == 10 && str[0] == '1') || (str.Length == 11 && str[0] == '-' && str[1] == '1'))
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
