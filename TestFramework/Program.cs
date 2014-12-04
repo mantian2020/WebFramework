bf26ef38ce12f4fc39abdb1860021ef3b906b3dc
mantian2020 <1665767273@qq.com>
2014-12-04 18:13:27 +0800

    CacheFramework测试成功

---
+++ TestFramework/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CacheFramework;
+using CacheFramework.Config;
+using CommonHelper;
+
+namespace TestFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //生成memcache.config
+            /*
+            MemCachedConfigInfo memCachedConfigInfo = new MemCachedConfigInfo();
+            memCachedConfigInfo.ApplyMemCached = true;
+            memCachedConfigInfo.IntConnections = 3;
+            memCachedConfigInfo.LocalCacheTime = 30000;
+            memCachedConfigInfo.MaintenanceSleep = 30;
+            memCachedConfigInfo.MaxConnections = 1024;
+            memCachedConfigInfo.MinConnections = 3;
+            memCachedConfigInfo.Nagle = true;
+            memCachedConfigInfo.PoolName = "wowo";
+            memCachedConfigInfo.ServerList = "127.0.0.1:11211";
+            memCachedConfigInfo.SocketConnectTimeout = 1000;
+            memCachedConfigInfo.SocketTimeout = 3000;
+            Console.WriteLine(SerializeHelper.Serialize(memCachedConfigInfo));
+            */
+            TianTianCache.CacheStrategy.AddObject("asdf","12123");
+            Console.WriteLine(TianTianCache.CacheStrategy.RetrieveObject("asdf"));
+            
+
+            Console.Read();
+        }
+    }
+}
