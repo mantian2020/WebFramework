5b899b578235acbab5b3d64234ccb3d797d11d63
mantian2020 <1665767273@qq.com>
2014-12-15 16:40:40 +0800

    添加菜单

---
+++ CoreFramework/Controllers/BasePageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CoreFramework.Controllers
+{
+    public class BasePageController:Controller
+    {
+        /// <summary>
+        /// 项目名
+        /// </summary>
+        private string ProjectName;
+        public BasePageController(string projectName)
+        {
+            this.ProjectName = projectName;
+        }
+        
+    }
+}
--- Shop.Template/Controllers/TemplateController.cs
+++ Shop.Template/Controllers/TemplateController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Shop.Template.Model;
 
 namespace Shop.Template.Controllers
 {
@@ -17,6 +18,14 @@ namespace Shop.Template.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult LeftMenu()
+        {
+            List<MenuEntity> lstMenu = new List<MenuEntity>();
+            lstMenu.Add(new MenuEntity() { Name = "权限管理", Url = "template/index" });
+            lstMenu.Add(new MenuEntity() { Name = "应用信息", Url = "template/index" });
+            return PartialView(lstMenu);
         }
     }
 }
